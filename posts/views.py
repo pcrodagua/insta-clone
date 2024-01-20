4b105fb9de53c58aa6d1178db50e05416cfbd186
ispcrodagua <is.pab.ces@gmail.com>
2024-01-20 14:49:46 -0600

    update(render-response): add html render support for posts

--- posts/views.py
+++ posts/views.py
@@ -4,65 +4,35 @@ from datetime import datetime
 
 posts = [
     {
-        'name': 'My Dog.',
-        'user': 'Yésica Cortes',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortaños',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/237/200/200'
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Khe.',
-        'user': 'Pink Woman',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/84/200/200'
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nautural web.',
-        'user': 'Pancho Villa',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/784/200/200'
-    },
-    {
-        'name': 'Photoshop',
-        'user': 'Maria Rodriguez',
-        'timestamp': datetime.now().strftime('%b %dth , %Y - %H:%M:%S hrs'),
-        'picture': 'https://apollo-virginia.akamaized.net/v1/files/wpp3ygn0meax-CO/image;s=850x0'
-
-    },
-    {
-        'name': 'Colores',
-        'user': 'Juan Romero',
-        'timestamp': datetime.now().strftime('%b %dth , %Y - %H:%M:%S hrs'),
-        'picture': 'https://cdn.britannica.com/70/191970-131-A85628DA/Color-wheel-light-color-spectrum.jpg'
-
-    },
-    {
-        'name': 'Travel to the moon',
-        'user': 'Joseph Rasinger',
-        'timestamp': datetime.now().strftime('%b %dth , %Y - %H:%M:%S hrs'),
-        'picture': 'https://petapixel.com/assets/uploads/2019/02/details-800x787.jpg'
-    },
-    {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
-    },
-    {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 
-def list_posts(request: HttpRequest) -> HttpResponse:
+def list_posts(request):
     """List existing posts."""
-    content = list()
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src="{picture}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
